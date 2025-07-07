3105d47ae45c7fec48380d664ff2edcf147d9bf0
Eben4real <ebenezer.igbinoba@stu.cu.edu.ng>
2025-07-07 17:23:49 +0100

    add function to deposit collateral

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -27,6 +27,76 @@ import { DecentralizedStableCoin } from "./DecentralizedStableCoin.sol";
  * @notice This contract is based on the MakerDAO DSS system
  */
 contract DSCEngine is ReentrancyGuard {
+
+    ///////////////////
+    // Errors
+    ///////////////////
+    error DSCEngine__TokenAddressesAndPriceFeedAddressesAmountsDontMatch();
+    error DSCEngine__NeedsMoreThanZero();
+    error DSCEngine__TokenNotAllowed(address token);
+    error DSCEngine__TransferFailed();
+    error DSCEngine__BreaksHealthFactor(uint256 healthFactorValue);
+    error DSCEngine__MintFailed();
+    error DSCEngine__HealthFactorOk();
+    error DSCEngine__HealthFactorNotImproved();
+
+    ///////////////////
+    // State Variables
+    ///////////////////
+
+    /// @dev Mapping of token address to price feed address
+    mapping(address collateralToken => address priceFeed) private s_priceFeeds;
+    /// @dev Amount of collateral deposited by user
+    mapping(address user => mapping(address collateralToken => uint256 amount)) private s_collateralDeposited;
+
+    ///////////////////
+    // Events
+    ///////////////////
+
+    /**
+     * @dev Emitted when collateral is deposited
+     * @dev indexed tells the EVM to copy that event parameter into the log’s topic list, 
+     * enabling cheap server-side filtering and fast dApp look-ups at the cost of a few extra gas 
+     * per event.
+     * @param user The address of the user depositing collateral
+     * @param token The address of the collateral token
+     * @param amount The amount of collateral being deposited
+    */
+    event CollateralDeposited(address indexed user, address indexed token, uint256 indexed amount);
+    // event CollateralRedeemed(address indexed redeemFrom, address indexed redeemTo, address token, uint256 amount); // if redeemFrom != redeemedTo, then it was liquidated
+
+    ///////////////////
+    // Modifiers
+    ///////////////////
+    modifier moreThanZero(uint256 amount) {
+        if (amount == 0) {
+            revert DSCEngine__NeedsMoreThanZero();
+        }
+        _;
+    }
+
+    modifier isAllowedToken(address token) {
+        if (s_priceFeeds[token] == address(0)) {
+            revert DSCEngine__TokenNotAllowed(token);
+        }
+        _;
+    }
+
+    ///////////////////
+    // Functions
+    ///////////////////
+    constructor(address[] memory tokenAddresses, address[] memory priceFeedAddresses, address dscAddress) {
+        if (tokenAddresses.length != priceFeedAddresses.length) {
+            revert DSCEngine__TokenAddressesAndPriceFeedAddressesAmountsDontMatch();
+        }
+        // These feeds will be the USD pairs
+        // For example ETH / USD or MKR / USD
+        for (uint256 i = 0; i < tokenAddresses.length; i++) {
+            s_priceFeeds[tokenAddresses[i]] = priceFeedAddresses[i];
+            // s_collateralTokens.push(tokenAddresses[i]);
+        }
+        i_dsc = DecentralizedStableCoin(dscAddress);
+    }
     // function depositCollateralAndMintDsc(
     //     address tokenCollateralAddress,
     //     uint256 amountCollateral,
@@ -38,22 +108,29 @@ contract DSCEngine is ReentrancyGuard {
     //     mintDsc(amountDscToMint);
     // }
 
-    //  function depositCollateral(
-    //     address tokenCollateralAddress,
-    //     uint256 amountCollateral
-    // )
-    //     public
-    //     moreThanZero(amountCollateral)
-    //     nonReentrant
-    //     isAllowedToken(tokenCollateralAddress)
-    // {
-    //     s_collateralDeposited[msg.sender][tokenCollateralAddress] += amountCollateral;
-    //     emit CollateralDeposited(msg.sender, tokenCollateralAddress, amountCollateral);
-    //     bool success = IERC20(tokenCollateralAddress).transferFrom(msg.sender, address(this), amountCollateral);
-    //     if (!success) {
-    //         revert DSCEngine__TransferFailed();
-    //     }
-    // }
+    /*
+     * @param tokenCollateralAddress: The ERC20 token address of the collateral you're depositing
+     * @param amountCollateral: The amount of collateral you're depositing
+    */
+    function depositCollateral(
+        address tokenCollateralAddress,
+        uint256 amountCollateral
+    )
+        public
+        moreThanZero(amountCollateral)
+        nonReentrant
+        isAllowedToken(tokenCollateralAddress)
+    {
+        s_collateralDeposited[msg.sender][tokenCollateralAddress] += amountCollateral;
+        emit CollateralDeposited(msg.sender, tokenCollateralAddress, amountCollateral);
+        /** @dev IERC20 is a metadata until it is pointed to a real address (tokenCollateralAddress)
+         * in this case, it is the address of the ERC20 token contract
+        */
+        bool success = IERC20(tokenCollateralAddress).transferFrom(msg.sender, address(this), amountCollateral);
+        if (!success) {
+            revert DSCEngine__TransferFailed();
+        }
+    }
 
     // function redeemCollateralForDsc(
     //     address tokenCollateralAddress,
